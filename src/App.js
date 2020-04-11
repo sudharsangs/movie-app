0e62edf898b0986d87a4ca7244fe00587e0e517a
Sudharsan G S <g.s.sudharsan99@gmail.com>
2020-04-11 22:08:01 +0530

    Completed search component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from 'react';
+import Search from './components/Search'
+import './App.css';
 
 function App() {
     return (
         <div>
-            
+            <header>
+                <h1>Movies App</h1>
+            </header>
+            <main>
+                <Search />
+            </main>
         </div>
     )
 }
